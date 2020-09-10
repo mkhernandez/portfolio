d87ba3314ce40173574624d4cb2839f6686d5e77
Mark Hernandez <markkimhernandez@gmail.com>
2020-09-09 22:09:57 -0500

    Created Partials Directory with header.ejs and footer.ejs files  and included files in home.ejs and register.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var route = express.Router();
 
 /* GET home page. */
 route.get('/', function(req, res, next) {
-  res.render('home.ejs');
+  res.render('home');
   // res.render('index', { title: 'Express' });
 });
 
--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', (req, res) => {
-    res.render('register.ejs');
+    res.render('register');
 });
 
 module.exports = route;
