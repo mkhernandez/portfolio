ff27af71f8e45a8e02e6a1e2b404f8ba7b6b3a13
Mark Hernandez <markkimhernandez@gmail.com>
2020-09-12 22:08:28 -0500

    Added Forms for Register and Login on Register Page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var route = express.Router();
 /* GET home page. */
 route.get('/', function(req, res, next) {
   res.render('home');
-  // res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = route;
