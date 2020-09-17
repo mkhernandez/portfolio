6050f88850425b8d874149f1161ec7059322a795
Mark Hernandez <markkimhernandez@gmail.com>
2020-09-16 22:56:42 -0500

    Finished Functionality of Todo List and Added Some Styling

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,12 +1,21 @@
 //Check off specific todo by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed"); 
 });
 
 //Click on X to delete the todo item
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     })
     event.stopPropagation();
+});
+
+//listener for input
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13){
+        let inputText = $(this).val();
+        $(this).val("");
+        $("#todoList").append("<li><span>X</span> " + inputText + "</li>");
+    }
 });
